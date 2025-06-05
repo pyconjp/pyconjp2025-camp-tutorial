f91ab1d6d09f5c4d9fa79a294b1ebc5e583a1b9b
Manabu TERADA <terada@cmscom.jp>
2025-06-06 08:04:58 +0900

    remove main func

--- main.py
+++ main.py
@@ -103,13 +103,3 @@ def single(data: SingleRequest):
             "meta": {"duration": duration},
         }
         return response
-
-
-def main():
-    import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
-if __name__ == "__main__":
-    main()
